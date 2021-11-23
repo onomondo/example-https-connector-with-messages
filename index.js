e3b06077c257e80c0756960c385bf65f8bda1353
Tobias Baunbaek <freeall@gmail.com>
2021-11-23 10:24:04 +0100

    Write to /messages

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const axios = require('axios')
+
+const API_KEY = process.env.API_KEY
 
 const app = express()
 
-app.post('/', express.json(), (req, res) => {
+axios.defaults.baseURL = 'https://api.onomondo.com'
+axios.defaults.headers.common['authorization'] = API_KEY
+
+app.post('/', express.json(), async (req, res) => {
   console.log(req.body)
-  res.send('Hello World!')
+  const { data } = await axios.post('/messages/000001935', {
+    protocol: 'udp',
+    port: 1234,
+    text: 'hello world'
+  })
+  console.log(data)
+  res.end()
 })
 
 app.listen(process.env.PORT || 3030)
+

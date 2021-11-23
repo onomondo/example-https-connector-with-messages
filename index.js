84b838fca3ae4168c3886302df46068e6f7e8b0a
Tobias Baunbaek <freeall@gmail.com>
2021-11-23 13:23:29 +0100

    Echo back to same UDP port that the device sent from

--- index.js
+++ index.js
@@ -9,15 +9,30 @@ axios.defaults.baseURL = 'https://api.onomondo.com'
 axios.defaults.headers.common['authorization'] = API_KEY
 
 app.post('/', express.json(), async (req, res) => {
-  console.log(req.body)
-  const { data } = await axios.post('/messages/000001935', {
+  const {
+    timestamp,
+    sim_id: simId,
+    payload,
+    header
+  } = req.body
+  const {
+    protocol,
+    destination_ip: destinationIp,
+    destination_port: destinationPort,
+    device_ip: deviceIp,
+    device_port: devicePort
+  } = header
+  console.log(`${timestamp} - ${simId} - ${protocol} - ${destinationIp}:${destinationPort} - ${deviceIp}:${devicePort}`)
+
+  if (protocol !== 'udp') return res.end()
+
+  await axios.post(`/messages/${simId}`, {
     protocol: 'udp',
-    port: 1234,
-    text: 'hello world'
+    port: devicePort,
+    data: Buffer.from(payload).toString('base64') // Echo back as a base64 encoded string
   })
-  console.log(data)
+
   res.end()
 })
 
 app.listen(process.env.PORT || 3030)
-
